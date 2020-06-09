cf50e447a288ab3fb5be6e6c20a9073b63054016
akhirudin <acerudyn@yahoo.com>
2020-06-09 15:18:03 +0700

    add helper store container

---
+++ app/Helper/Jbi/MasterContainer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helper\Jbi;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MasterContainer{
+
+	public static function storeMasterContainer($input){
+		$cont_no = $input['CONT_NO'];
+		$check = DB::connection('mdm_ilcs')->table('TM_CONTAINER')->where('cont_no', $cont_no)->count();
+		// dd($check);
+		if ($check > 0) {
+			return [ "result" => "Error, nomor container sudah ada", "cont_no"=> $cont_no ];
+		} else {
+			DB::connection('mdm_ilcs')->table('TM_CONTAINER')->insert([
+				'CONT_NO' => $input['CONT_NO'],
+				'CONT_SIZE' => $input['CONT_SIZE'],
+				'CONT_TYPE' => $input['CONT_TYPE'],
+				'CONT_DANGER' => $input['CONT_DANGER'],
+				'CONT_STATUS' => $input['CONT_STATUS'],
+				'COMMODITY' => $input['COMMODITY'],
+				'CARGO_OWNER' => $input['CARGO_OWNER'],
+			]);
+		return [ "result" => "Success, store master container", "cont_no"=> $cont_no ];
+		}
+
+	}
+
+}
